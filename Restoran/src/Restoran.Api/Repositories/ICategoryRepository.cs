168565c3771f61616bb9bde093257f654db94727
Miryoqub006 <158826997+Miryoqub006@users.noreply.github.com>
2026-06-13 16:11:17 +0500

    Continue

--- Restoran/src/Restoran.Api/Repositories/ICategoryRepository.cs
+++ Restoran/src/Restoran.Api/Repositories/ICategoryRepository.cs
@@ -2,5 +2,12 @@
 
 public interface ICategoryRepository
 {
+    public Task GetAllAsync();
+    public Task<Dto> GetByIdAsync(long id);
 
+    public Task<Dto> AddAsync(Dto category);
+
+    public Task<long> UpdateAsync(Dto category);
+    public Task DeleteAsync(long id);
+    
 }
