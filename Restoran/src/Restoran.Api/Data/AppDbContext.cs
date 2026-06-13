2a7a1bb6454abe6616069df59870f056802cf267
Miryoqub006 <158826997+Miryoqub006@users.noreply.github.com>
2026-06-13 15:58:06 +0500

    Done

---
+++ Restoran/src/Restoran.Api/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restoran.Api.Entities;
+using System.Reflection;
+
+namespace Restoran.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Food> Foods { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
